f5f44f8ac25b8dafb01a676b3fae02fa05af827e
chandra shekar kanukuntla <119240540+chandrashekar21041997@users.noreply.github.com>
2023-09-06 05:06:03 +0530

    provider.tf

--- provider.tf
+++ provider.tf
@@ -10,6 +10,5 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIA5DHMLTA4H2OD5XBH" 
-  secret_key = "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"
+
 }
